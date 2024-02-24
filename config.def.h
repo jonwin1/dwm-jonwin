23ffe44c4b93eb7911314524134986f5aeb63ab4
jonwin1 <jonatan.wincent@proton.me>
2024-02-24 18:17:41 +0100

    xresources

--- config.def.h
+++ config.def.h
@@ -1,26 +1,28 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static unsigned int borderpx        = 1;        /* border pixel of windows */
+static unsigned int snap            = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static const int showbar            = 0;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static int showbar                  = 0;        /* 0 means no bar */
+static int topbar                   = 1;        /* 0 means bottom bar */
 static const double activeopacity   = 1.0f;     /* Window opacity when it's focused (0 <= opacity <= 1) */
 static const double inactiveopacity = 0.7f;     /* Window opacity when it's inactive (0 <= opacity <= 1) */
-static const char *fonts[]          = { "monospace:size=16" };
-static const char dmenufont[]       = "monospace:size=16";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static char font[]                  = "monospace:size=10" ;
+static char dmenufont[]             = "monospace:size=10";
+static const char *fonts[]          = { font };
 static const unsigned int baralpha  =   0xb4;
 static const unsigned int borderalpha = 0xb4;
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+static char normbgcolor[]           = "#222222";
+static char normbordercolor[]       = "#444444";
+static char normfgcolor[]           = "#bbbbbb";
+static char selfgcolor[]            = "#eeeeee";
+static char selbordercolor[]        = "#005577";
+static char selbgcolor[]            = "#005577";
+static char *colors[][3] = {
+       /*               fg           bg           border   */
+       [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+       [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };
 static const unsigned int alphas[][3]      = {
     /*               fg      bg        border*/
@@ -44,9 +46,9 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
+static float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
+static int nmaster     = 1;    /* number of clients in master area */
+static int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 0; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
@@ -69,8 +71,29 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+		{ "font",               STRING,     &font },
+		{ "font",               STRING,     &dmenufont },
+		{ "background",         STRING,     &normbgcolor },
+		{ "background",         STRING,     &normbordercolor },
+		{ "foreground",         STRING,     &normfgcolor },
+		{ "color1",             STRING,     &selbgcolor },
+		{ "color1",             STRING,     &selbordercolor },
+		{ "foreground",         STRING,     &selfgcolor },
+		{ "borderpx",          	INTEGER,    &borderpx },
+		{ "snap",          		INTEGER,    &snap },
+		{ "showbar",          	INTEGER,    &showbar },
+		{ "topbar",          	INTEGER,    &topbar },
+		{ "nmaster",          	INTEGER,    &nmaster },
+		{ "resizehints",       	INTEGER,    &resizehints },
+		{ "mfact",      	 	FLOAT,      &mfact },
+};
 
 #include "exitdwm.c"
 static const Key keys[] = {
