6289ba5a59f48fa120dce88ea3c8ab749cb847a2
jonwin1 <jonatan.wincent@proton.me>
2024-03-04 19:20:01 +0100

    scratchpad

--- config.def.h
+++ config.def.h
@@ -78,6 +78,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char scratchpadname[] = "scratchpad";
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 
 /*
  * Xresources preferences to load at startup
@@ -121,6 +123,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_p,       spawn,                  {.v = dmenucmd } },
 	{ MODKEY,                       XK_q,       killclient,             {0} },
 	{ MODKEY|ShiftMask,             XK_q,       exitdwm,                {0} },
+	{ MODKEY,                       XK_s,       togglescratch,          {.v = scratchpadcmd } },
     { MODKEY|ShiftMask,             XK_s,       changefocusopacity,     {.f = -0.025}},
     { MODKEY|ShiftMask,             XK_t,       setlayout,              {.v = &layouts[0]} },
 	{ MODKEY,                       XK_u,       setlayout,              {.v = &layouts[3]} },
