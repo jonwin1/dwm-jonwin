45c8a99bff40c09958eb5ff73cdba6903a1e6f9c
jonwin1 <jonatan.wincent@proton.me>
2024-03-17 20:15:54 +0100

    tilewide

--- config.def.h
+++ config.def.h
@@ -28,13 +28,14 @@ static const Rule rules[] = {
 	{ NULL,             NULL,       "Event Tester", 0,          0,          0,          1,          0,          -1 }, /* xev */
 };
 
-static const float mfact        = 0.333;    /* factor of master area size [0.05..0.95] */
-static const int nmaster        = 1;        /* number of clients in master area */
+static const float mfact        = 0.666;    /* factor of master area size [0.05..0.95] */
+static const int nmaster        = 2;        /* number of clients in master area */
 static const int resizehints    = 0;        /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1;        /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
+	{ "[][]=",    tilewide },
 	{ "|M|",      centeredmaster },
 	{ "[]=",      tile },
 	{ "><>",      NULL },
