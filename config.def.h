730fe73d761a0dcb2264d29bdda5d3e0aa9f538b
jonwin1 <jonatan.wincent@google.com>
2024-05-20 20:34:17 +0200

    tilewide

--- config.def.h
+++ config.def.h
@@ -42,6 +42,7 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
+	{ "[][]=",    tilewide },
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
@@ -103,9 +104,10 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_q,       killclient,     {0} },
 	{ MODKEY|ShiftMask,             XK_q,       exitdwm,       {0} },
 
-	{ MODKEY,                       XK_t,       setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,       setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,       setlayout,      {.v = &layouts[2]} },
+	{ MODKEY|ShiftMask,             XK_t,       setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_t,       setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_f,       setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_m,       setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       XK_space,   setlayout,      {0} },
 
 	{ MODKEY,                       XK_0,       view,           {.ui = ~0 } },
