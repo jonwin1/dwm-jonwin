493866482082aa2bcbd341c1ad9f5e1c92c4ed8f
jonwin1 <jonatan.wincent@gmail.com>
2024-05-30 14:41:39 +0200

    improve zoom keybind

--- config.def.h
+++ config.def.h
@@ -101,7 +101,7 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_j,       movestack,      {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_k,       movestack,      {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_space,   togglefloating, {0} },
-	{ MODKEY|ShiftMask,             XK_Return,  zoom,           {0} },
+	{ MODKEY,                       XK_Return,  zoom,           {0} },
 	{ MODKEY,                       XK_q,       killclient,     {0} },
 
   // layout
